cd5ee0ba1590c61bc2ddb0b12b9db541d1bf148c
Admin <Admin@NJ.mshome.net>
2024-03-11 11:31:45 +0530

    this is my first project

---
+++ CoreJava/src/Day1/Sample.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+            int a=10,b=8;
+            int c=a+b;
+            System.out.println("the addition of two number:" +c);
+
+	}
+
+}
