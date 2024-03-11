105c4f844adf6041aad8b849c55f4dbc37e28648
Mugundan28 <162946419+Mugundan28@users.noreply.github.com>
2024-03-11 15:54:41 +0530

    nextline

---
+++ Sample2.java
@@ -0,0 +1,14 @@
+package Day1;
+import java.util.*;
+public class Sample2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String n= input.nextLine();
+		System.out.println("Hello " +n);
+		
+
+	}
+
+}
